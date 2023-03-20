249e194fffeb024aaf5834bc7862f9f4aca473d8
danschnurpfeil <dschnurp@email.cz>
2023-03-21 00:10:53 +0100

    fresh project

--- web_app/manage.py
+++ web_app/manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TowardsDataScienceWeb.settings')
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'towards_data_science.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- web_app/towards_data_science/wsgi.py
+++ web_app/towards_data_science/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for web project.
+WSGI config for towards_data_science project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TowardsDataScienceWeb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'towards_data_science.settings')
 
 application = get_wsgi_application()
